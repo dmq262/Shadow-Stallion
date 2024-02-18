0052a7d764f999baeb3339aeb4c9b9444dbfb834
Tim Lin <lintim03@gmail.com>
2024-02-17 23:12:48 -0600

    initialize main scene with music

---
+++ scenes/main.gd
@@ -0,0 +1,13 @@
+extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	game_start()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func game_start():
+	$music.play()
