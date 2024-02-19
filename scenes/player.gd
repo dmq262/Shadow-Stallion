6afd83f06cb73c8be80df14bbedef6ff66e009a8
Tim Lin <lintim03@gmail.com>
2024-02-19 15:37:37 -0600

    added dashing mechanic

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 const SPEED = 300
+var dash_vector = Vector2(0, 0)
+var dash_power = 20
 var old_playback = 0
 
 func _ready():
@@ -18,8 +20,14 @@ func _physics_process(delta):
 		velocity.y += 1
 	if Input.is_action_pressed("move_up"):
 		velocity.y -=1
-		
-	var collision = move_and_collide(velocity * delta * SPEED)
+	
+	#Implement Dash Mechanic
+	if Input.is_action_just_pressed("dash"):
+		dash_vector = velocity.normalized() * dash_power
+	if dash_vector.length() > .01:
+		dash_vector -= delta * dash_vector.normalized() * 50
+	
+	var collision = move_and_collide(velocity.normalized() * delta * SPEED + dash_vector)
 	
 	#PROCESS PLAYER SOUNDS
 	if velocity.length() > 0 and not $sound_footsteps.playing:
