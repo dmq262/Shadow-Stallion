9fb9b55379a1be3716250508466aef5466c89346
Tim Lin <lintim03@gmail.com>
2024-02-23 01:15:57 -0600

    enemies in room, active only when player enters

--- scenes/enemy_ranged.gd
+++ scenes/enemy_ranged.gd
@@ -3,21 +3,26 @@ extends CharacterBody2D
 @export var bullet_scene: PackedScene
 
 const SPEED = 300.0
+var active = false
 var player
 var cooldown = 3
 var cooldown_progress = 3
 
 func _ready():
+	cooldown_progress = randf_range(0, cooldown)
+	active = false
 	player = get_tree().current_scene.get_node('player')
 	if player == null:
 		print('player not found')
 
 func _physics_process(delta):
-	if player == null:
+	#Do nothing if unable to find player, or not active
+	if player == null or (not active):
 		return
 		
 	look_at(player.position)
 	
+	#Shoot at player
 	if cooldown_progress <= 0:
 		shoot_bullet()
 		cooldown_progress = cooldown
@@ -25,6 +30,7 @@ func _physics_process(delta):
 		cooldown_progress -= delta
 		
 
+#Helper Function for shooting at player
 func shoot_bullet():
 	var bullet = bullet_scene.instantiate()
 	bullet.global_position = $gun_tip.global_position
--- scenes/room.gd
+++ scenes/room.gd
@@ -13,4 +13,6 @@ func _process(delta):
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
-		pass
+		for node in get_children():
+			if node.is_in_group('enemy'):
+				node.active = true
