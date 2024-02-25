a49561c9a50ed6302be81a8f8178acf6d0eab8f2
Tim Lin <lintim03@gmail.com>
2024-02-24 21:48:37 -0600

    added sword mechanic: blocking and hitting

--- scenes/player.gd
+++ scenes/player.gd
@@ -7,6 +7,8 @@ var dash_vector = Vector2(0, 0)
 var dash_power = 15
 var old_playback = 0
 var health = 100
+var bullets_in_range = []
+var enemies_in_range = []
 
 func _ready():
 	pass
@@ -27,6 +29,15 @@ func _physics_process(delta):
 		bullet.add_to_group("player_bullet")
 		get_tree().current_scene.add_child(bullet)
 		
+	#Block/Parry/Swing Sword
+	if Input.is_action_just_pressed('block'):
+		for enemy_bullet in bullets_in_range:
+			bullets_in_range.erase(enemy_bullet)
+			enemy_bullet.queue_free()
+			
+		for enemy in enemies_in_range:
+			enemy.health -= 100
+			
 	
 	#Movement
 	var velocity = Vector2.ZERO
@@ -71,3 +82,19 @@ func _physics_process(delta):
 func die():
 	hide()
 
+#SWORD AREA ON ENTER AND EXIT
+func _on_sword_hitbox_area_entered(area):
+	if area.is_in_group('enemy_bullet'):
+		bullets_in_range.append(area)
+
+func _on_sword_hitbox_area_exited(area):
+	if area.is_in_group('enemy_bullet') and bullets_in_range.has(area):
+		bullets_in_range.erase(area)
+
+func _on_sword_hitbox_body_entered(body):
+	if body.is_in_group('enemy'):
+		enemies_in_range.append(body)
+
+func _on_sword_hitbox_body_exited(body):
+	if body.is_in_group('enemy') and enemies_in_range.has(body):
+		enemies_in_range.erase(body)
