9d2ac7e2675f1323f2de87e799efde0cb9b067ea
Tim Lin <lintim03@gmail.com>
2024-02-25 13:27:38 -0600

    Added temporary restart mechanic

--- scenes/main.gd
+++ scenes/main.gd
@@ -7,7 +7,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("restart"):
+		for wall in get_tree().get_nodes_in_group('wall'):
+			wall.queue_free()
+		for room in get_tree().get_nodes_in_group('room'):
+			room.queue_free()
+		$player.health = 100
+		$player.show()
+		$player.global_position = Vector2(0, 0)
+		game_start()
 
 func game_start():
 	$music.play()
