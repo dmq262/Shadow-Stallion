866386724edb079ce4aefd2ea2c6ee3dd6049a32
Tim Lin <lintim03@gmail.com>
2024-02-27 22:07:32 -0600

    added loot

---
+++ scenes/loot.gd
@@ -0,0 +1,47 @@
+extends Area2D
+
+var player
+var velocity = Vector2.ZERO
+var acceleration = 10
+var type = "ammo"
+var value = 20
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	global_position += velocity
+	
+	#Pick up if close enough
+	if player and (player.global_position - global_position).length() < 30:
+		player_pickup()
+	#If in range, move towards player(scales with proximity
+	elif player:
+		velocity += ((player.global_position - global_position).normalized() * acceleration * delta) / ((player.global_position - global_position).length() / 50)
+	#If not in range, slow down
+	elif velocity.length() != 0:
+		var old_vel = velocity
+		velocity -= velocity.normalized() * delta * acceleration
+		
+		if old_vel.length() < velocity.length():
+			velocity = Vector2.ZERO
+		
+
+
+func player_pickup():
+	if type == "health":
+		player.health += value
+	elif type == "ammo":
+		player.ammo += value
+	else:
+		player.expirience += value
+	
+	queue_free()
+
+
+func _on_body_entered(body):
+	if body.is_in_group('player'):
+		player = body
+
+
+func _on_body_exited(body):
+	if body.is_in_group('player'):
+		player = null
