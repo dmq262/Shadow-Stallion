795222c1ff4c58eab23ad0c2ecca8e1338de2a9d
Tim Lin <lintim03@gmail.com>
2024-02-29 18:38:04 -0600

    added stats GUI

--- scenes/hud.gd
+++ scenes/hud.gd
@@ -1,5 +1,6 @@
 extends CanvasLayer
 
+signal upgrade_stat()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -7,14 +8,14 @@ func _process(delta):
 		pass
 
 func set_expirience(current, max):
-	$expirience_bar.ratio = float(current)/max
+	$gameplay/expirience_bar.ratio = float(current)/max
 
 func set_health(current, max):
-	$health_bar.ratio = float(current)/max
+	$gameplay/health_bar.ratio = float(current)/max
 
 func set_ammo(ammo):
 	var count = 0
-	for ammo_texture in $ammo_container.get_children():
+	for ammo_texture in $gameplay/ammo_container.get_children():
 		if ammo > count:
 			ammo_texture.show()
 		else:
@@ -22,10 +23,34 @@ func set_ammo(ammo):
 		count += 1
 
 func set_gun_cooldown(current, max):
-	$gun_cooldown.ratio = float(current)/max
+	$gameplay/gun_cooldown.ratio = float(current)/max
 
 func set_sword_cooldown(current, max):
-	$sword_cooldown.ratio = float(current)/max
+	$gameplay/sword_cooldown.ratio = float(current)/max
 
 func set_dash_cooldown(current, max):
-	$dash_cooldown.ratio = float(current)/max
+	$gameplay/dash_cooldown.ratio = float(current)/max
+
+
+#UPGRADE BUTTONS
+func upgrade_health():
+	emit_signal('upgrade_stat', 'health')
+	
+func upgrade_speed():
+	emit_signal('upgrade_stat', 'speed')
+	
+func upgrade_ammo():
+	emit_signal('upgrade_stat', 'ammo')
+	
+func upgrade_gun_cooldown():
+	emit_signal('upgrade_stat', 'gun_cooldown')
+	
+func upgrade_bullet_speed():
+	emit_signal('upgrade_stat', 'bullet_speed')
+	
+func upgrade_bullet_damage():
+	emit_signal('upgrade_stat', 'bullet_damage')
+	
+func upgrade_bullet_size():
+	emit_signal('upgrade_stat', 'bullet_size')
+
